be7a5206945d6911f1ba2c7ea4dde8319211199f
Bijoy Anil <86096110+bijoy7034@users.noreply.github.com>
2022-07-14 19:35:32 +0530

    3

--- Certificate/create.js
+++ Certificate/create.js
@@ -1,6 +1,7 @@
 function createPdf(){
 
     var namesArr = [];
+    var deptname =[];
 
     Papa.parse(document.querySelector('#formFile').files[0],
     {
@@ -8,13 +9,14 @@ function createPdf(){
         header : true,
         skipEmpty: true,
         complete : function(results){
-            console.log(results.data[1].FullName);
+            console.log(results.data[1].Deptname);
             for(i =0 ; i < results.data.length;i++){
                 namesArr.push(results.data[i].FullName);
+                deptname.push(results.data[i].Deptname);
             }
             console.log(namesArr[2]);
-            for(j=0; j<=namesArr.length;j++){
-                generatePDF(String(namesArr[j]));
+            for(j=0; j<namesArr.length;j++){
+                generatePDF(String(namesArr[j]),String(deptname[j]));
             }
             
         }
@@ -23,9 +25,9 @@ function createPdf(){
 
 
 
-    const generatePDF= async (name)=>{
+    const generatePDF= async (name,deptname)=>{
         const {PDFDocument, rgb} = PDFLib;
-        const exBytes = await fetch ("saint' .pdf").then((res) =>{
+        const exBytes = await fetch ("5.pdf").then((res) =>{
             return res.arrayBuffer();
         });
     
@@ -40,10 +42,16 @@ function createPdf(){
         const pages = PDFDoc.getPages();
         const firstPg = pages[0];
         firstPg.drawText(name,{
-            x: 446,
-            y:790,
-            size: 30,
+            x: 246,
+            y:398,
+            size: 15,
         });
+        firstPg.drawText(deptname,{
+            x: 166,
+            y:378,
+            size:15,
+        });
+        
     
     
         const uri = await PDFDoc.saveAsBase64({dataUri : true});
